64b27d7f8e29e8d9de379859148f8973977df178
serleo98 <33487419+serleo98@users.noreply.github.com>
2024-10-29 17:27:07 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const path = require('path');
+const fetch = require('node-fetch');
 const indexRouter = require('./routes/index');
 
 const app = express();
 const PORT = 3000;
+
+// Middleware to parse JSON
+app.use(express.json());
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
@@ -11,10 +15,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Use the router for handling routes
 app.use('/', indexRouter);
 
+// Endpoint to receive a URL and consume it
+app.post('/consume-url', async (req, res) => {
+  const { url } = req.body;
+  try {
+    const response = await fetch(url);
+    const data = await response.text();
+    res.status(response.status).send(data);
+  } catch (error) {
+    res.status(500).send('Error consuming the URL');
+  }
+});
+
 // Catch-all route for handling 404 errors
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-  });
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
